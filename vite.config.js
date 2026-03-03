5bb9b060918537ded04b6b832b0ec200fba509df
Ji Hae Choi <jiha9139@gmail.com>
2026-03-02 23:03:59 -0600

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: Change 'commune-app' to your actual GitHub repo name
-  base: '/commune-app/',
+  base: "/"
 })
